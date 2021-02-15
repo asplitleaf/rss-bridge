c12df83c19fd365d1e6be336f0d737a1a0fd3f53
asplitleaf <asplitleaf@gmail.com>
2021-02-15 13:10:44 -0800

    first init

---
+++ bridges/AnkRougeBridge.php
@@ -0,0 +1,28 @@
+<?php
+class AnkRougeBridge extends BridgeAbstract {
+
+	const NAME = 'Ank Rouge';
+	const URI = 'http://blog.ailand-store.jp/ar0164/';
+	const DESCRIPTION = 'Returns staff posts from Ank Rouge Ailand blog';
+	const MAINTAINER = 'asplitleaf';
+
+	public function collectData() {
+		$html = getSimpleHTMLDOMCached(self::getURI())
+			or returnServerError('Could not load content');
+
+		foreach($html->find('.section') as $element) {
+			$item = array();
+
+			$snippet = $element->find('.content .text', 0);
+			$link = $element->find('.title a', 0);
+
+			$item['title'] = $link->plaintext;
+			$item['content'] = $snippet->innertext;
+			$item['uri'] = $link->href;
+			$item['timestamp'] = strtotime($element->find('p.date', 0)->plaintext);
+
+			$this->items[] = $item;
+		}
+
+	}
+}
